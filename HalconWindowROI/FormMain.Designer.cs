aad3581743c1de3b7c365244f808731ee9ff88f7
atway <zhaitianyong@artwin.cn>
2020-11-23 19:16:37 +0800

    修复了加载图片不显示的buginit

---
+++ HalconWindowROI/FormMain.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace HalconWindowROI
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hWindowControl1 = new HalconDotNet.HWindowControl();
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.btnDrawRect1 = new System.Windows.Forms.Button();
+            this.btnDrawRect2 = new System.Windows.Forms.Button();
+            this.btnDrawCircle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hWindowControl1
+            // 
+            this.hWindowControl1.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl1.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl1.ImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.hWindowControl1.Location = new System.Drawing.Point(12, 42);
+            this.hWindowControl1.Name = "hWindowControl1";
+            this.hWindowControl1.Size = new System.Drawing.Size(569, 381);
+            this.hWindowControl1.TabIndex = 0;
+            this.hWindowControl1.WindowSize = new System.Drawing.Size(569, 381);
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new System.Drawing.Point(614, 57);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadImage.TabIndex = 1;
+            this.btnLoadImage.Text = "加载图片";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+            // 
+            // btnDrawRect1
+            // 
+            this.btnDrawRect1.Location = new System.Drawing.Point(614, 113);
+            this.btnDrawRect1.Name = "btnDrawRect1";
+            this.btnDrawRect1.Size = new System.Drawing.Size(75, 23);
+            this.btnDrawRect1.TabIndex = 2;
+            this.btnDrawRect1.Text = "绘制矩形1";
+            this.btnDrawRect1.UseVisualStyleBackColor = true;
+            this.btnDrawRect1.Click += new System.EventHandler(this.btnDrawRect1_Click);
+            // 
+            // btnDrawRect2
+            // 
+            this.btnDrawRect2.Location = new System.Drawing.Point(614, 158);
+            this.btnDrawRect2.Name = "btnDrawRect2";
+            this.btnDrawRect2.Size = new System.Drawing.Size(75, 23);
+            this.btnDrawRect2.TabIndex = 3;
+            this.btnDrawRect2.Text = "绘制矩形2";
+            this.btnDrawRect2.UseVisualStyleBackColor = true;
+            this.btnDrawRect2.Click += new System.EventHandler(this.btnDrawRect2_Click);
+            // 
+            // btnDrawCircle
+            // 
+            this.btnDrawCircle.Location = new System.Drawing.Point(614, 211);
+            this.btnDrawCircle.Name = "btnDrawCircle";
+            this.btnDrawCircle.Size = new System.Drawing.Size(75, 23);
+            this.btnDrawCircle.TabIndex = 4;
+            this.btnDrawCircle.Text = "绘制圆形";
+            this.btnDrawCircle.UseVisualStyleBackColor = true;
+            this.btnDrawCircle.Click += new System.EventHandler(this.btnDrawCircle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDrawCircle);
+            this.Controls.Add(this.btnDrawRect2);
+            this.Controls.Add(this.btnDrawRect1);
+            this.Controls.Add(this.btnLoadImage);
+            this.Controls.Add(this.hWindowControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private HalconDotNet.HWindowControl hWindowControl1;
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.Button btnDrawRect1;
+        private System.Windows.Forms.Button btnDrawRect2;
+        private System.Windows.Forms.Button btnDrawCircle;
+    }
+}
+
---
+++ HalconWindowROI/FormMain.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HalconDotNet;
+using ViewROI;
+
+namespace HalconWindowROI
+{
+    public partial class FormMain : Form
+    {
+        HWndCtrl _viewCtrl;
+        ROIController _roiCtrl;
+        HImage hImage;
+        public FormMain()
+        {
+            InitializeComponent();
+
+         
+
+
+        }
+
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.RestoreDirectory = true;
+            dialog.Multiselect = false;
+            dialog.Filter = "图片|*.*";
+            dialog.Title = "选择一张图片";
+            DialogResult result =  dialog.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                hImage = new HImage(dialog.FileName);
+                _viewCtrl.addIconicVar(hImage); 
+                //_viewCtrl.resetWindow();
+                //_viewCtrl.repaint();
+                _viewCtrl.setViewState(HWndCtrl.MODE_VIEW_ZOOM_Wheel);
+            }
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _viewCtrl = new HWndCtrl(hWindowControl1);
+            _roiCtrl = new ROIController();
+            _viewCtrl.useROIController(_roiCtrl);
+            _roiCtrl.viewController.viewPort.HMouseMove += ViewPort_HMouseMove; ;
+        }
+
+        private void ViewPort_HMouseMove(object sender, HMouseEventArgs e)
+        {
+            var roi = _roiCtrl.getActiveROI();
+            if (roi == null) return;
+     
+            for (var i = 0; i < roi.getModelDataName().Length; i++)
+            {
+
+                Debug.WriteLine($"{roi.getModelDataName()[i].S} : {roi.getModelData()[i].D}");
+            }
+        }
+
+        private void btnDrawRect1_Click(object sender, EventArgs e)
+        {
+            ROIRectangle1 rect1 = new ROIRectangle1();
+            _roiCtrl.setROIShape(rect1);
+            
+        }
+
+        
+        private void btnDrawRect2_Click(object sender, EventArgs e)
+        {
+            ROIRectangle2 rect2 = new ROIRectangle2();
+            _roiCtrl.setROIShape(rect2);
+        }
+
+        private void btnDrawCircle_Click(object sender, EventArgs e)
+        {
+            ROICircle circle = new ROICircle();
+            _roiCtrl.setROIShape(circle);
+        }
+    }
+}
